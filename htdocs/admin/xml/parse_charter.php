7ffd5f5e14b16fa2e2d40f97493f67415ee4fb6c
Bill Hunt <bill.hunt@willowtreeapps.com>
2013-10-02 15:32:28 -0400

    Adding documentation to charter_parser.php

--- htdocs/admin/xml/parse_charter.php
+++ htdocs/admin/xml/parse_charter.php
@@ -1,16 +1,28 @@
 <?php
+/*
+ * Charter Parser
+ *
+ * When running this script, it will occasionally fail.
+ * So far, this seems to mainly happen due to it misinterpretting
+ * the beginning of a new section (Line 40 below).  This happens
+ * when a line begins with &#167 but is not actually the start of
+ * a new section.  The easiest solution is to remove the newline
+ * immediately before the &#167, bringing it back to the end of
+ * the previous line.
+ */
+
 	ini_set("error_reporting", E_ALL);
 	require_once('class.Section.php');
-	
+
 	file_put_contents('error.log', '');
-	
+
 	function customLog($msg){
 		 file_put_contents('error.log', "\n------------------\n" . print_r($msg, true) . "\n------------------\n", FILE_APPEND);
 	}
-	
+
 	$structure = array('@\n\s*(\([a-z]+\))\s.*@', '@\n\s*(\(\d+\)).*@', '@\n\s*(\([ixv]+\)).*@', '@\n\s*(\d+)\..*@');
 	$lawTopPattern = '@^\s*\(?(\d+(?:\.\d)?[A-Z]?)\.?\)?\s(.*)@';
-	
+
 	try{
 		$src = file_get_contents('../original/01 - Charter.xml');
 
@@ -19,28 +31,28 @@
 		foreach($articles as $index => $article){
 			if($index == 0){continue;}
 			$article = strip_tags($article);
-			
+
 			$isMatch = preg_match('/^Article ([IXV]{0,10})\s*(\S.*)/m', $article, $titleStuff);
-			
+
 			if($isMatch){
 				$art_num = $titleStuff[1];
 				$art_name = $titleStuff[2];
-				
+
 				//file_put_contents("Article_$art_num.txt", $article);
 			}else{
 				throw new Exception("Could not retrieve article info");
 			}
-			
+
 			preg_match_all("@\n&#167;(.*)\n@", $article, $lawMetas);
 			$lawStrings = preg_split("@\n&#167;.*\n@", $article, -1, PREG_SPLIT_DELIM_CAPTURE);
-			
-			
+
+
 			foreach($lawStrings as $lawIndex => $lawString){
 				if($lawIndex == 0){
 					continue;
 				}
-				
-				
+
+
 				$law = simplexml_load_string('<?xml version="1.0" encoding="utf-8"?><law></law>');
 				$structureNode = $law->addChild("structure");
 				$unit = $structureNode->addChild("unit", $titleStuff[2]);
@@ -48,13 +60,13 @@
 				$unit->addAttribute("identifier", $titleStuff[1]);
 				$unit->addAttribute("order_by", $index);
 				$unit->addAttribute("level", '1');
-				
-				$lawInfo = $lawMetas[1][$lawIndex -1];	
-				
+
+				$lawInfo = $lawMetas[1][$lawIndex -1];
+
 				//Only occurs with repealed laws
 				if(!isset($lawString) || $lawString == '' || preg_match("@(\d+)?(?:\sto)?\s(\d+)\.\s+{(.*)}@", $lawInfo)){
 					$repealedInfo = preg_match_all("@(\d+)?(?:\sto)?\s(\d+)\.\s+{(.*)}@", $lawInfo, $repealedLaws);
-					
+
 					//There was only one law repealed, not a set
 					if($repealedLaws[1][0] == ''){
 						$repealedNum = $repealedLaws[2][0];
@@ -68,7 +80,7 @@
 						$repealedNode = $metadata->addChild('repealed', 'y');
 					}else{//There is a range of repealed laws
 						$repealedText = $repealedLaws[3][0];
-						
+
 						for($i = $repealedLaws[1][0]; $i <= $repealedLaws[2][0]; $i++){
 							$repealedNum = $i;
 							$filename = $art_num . '-' . $repealedNum . '.xml';
@@ -78,17 +90,17 @@
 							$text = $law->addChild('text', $repealedText);
 							$metadata = $law->addChild('metadata');
 							$repealedNode = $metadata->addChild('repealed', 'y');
-							
+
 							$law->asXml($filename);
 							exec("xmllint -format $filename -output $filename");
 						}
-						
+
 						continue;
 					}
-					
+
 				}else{//Not repealed
 					$oldInfo = $lawInfo;
-					preg_match_all($lawTopPattern, $lawInfo, $lawInfo);	
+					preg_match_all($lawTopPattern, $lawInfo, $lawInfo);
 					if(!isset($lawInfo[1][0]) || $lawInfo[1][0] == ''){
 						customLog("Old Info: " . print_r($oldInfo, true));
 						customLog($lawInfo);
@@ -106,7 +118,7 @@
 					$metadata = $law->addChild('metadata');
 					$repealedNode = $metadata->addChild('repealed', 'n');
 				}
-				
+
 				$law->asXml($filename);
 				exec("xmllint -format $filename -output $filename");
 			}
@@ -118,4 +130,3 @@
 		echo "*************\n";
 		exit;
 	}
-	
